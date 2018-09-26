c630bf7d580d3bd53dabe26d701671a864ebd9f4
Burak <burak.alakuss@gmail.com>
2018-09-26 22:30:19 +0300

    Initial commit

---
+++ Assets/AdventureGame.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour {
+
+    [SerializeField] Text textComponent;
+
+
+	// Use this for initialization
+	void Start () {
+        textComponent.text = "Welcome to My Game";
+        textComponent.alignment = TextAnchor.MiddleCenter;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
